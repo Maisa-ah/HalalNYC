630f15ebfe1c35c8210c977e52c0d54e7c0ef09e
Maisa Ahmad <2013maisaah@gmail.com>
2023-06-05 20:09:01 -0400

    added database credentials

--- database.php
+++ database.php
@@ -1,61 +1,71 @@
 <?php
+  require_once 'vendor/autoload.php';
+  // use Dotenv\Dotenv;
+  $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+  // $dotenv = new Dotenv(__DIR__);
+  // try {
+    $dotenv->load();
+  // }
+  // catch (InvalidPathException $e){
+  //   echo "Failed to load env file.";
+  // }
 
-  $servername = "localhost:3308";
-  $username = "dev15dbuser";
-  $password = "FM15web2020";
-  $db = "dev15db";
+  $servername = $_ENV["DB_SERVERNAME"];
+  $username = $_ENV["DB_USERNAME"];
+  $password = $_ENV["DB_PASSWORD"];
+  $db = $_ENV["DB_NAME"];
+  echo 'SERVER' . $servername . $db;
+  // Create connection
+  $conn = mysqli_connect($servername, $username, $password, $db);
+  // mysql_select_db($db) or die("Could not open the database '$db'");
+  // Check connection
+  if (!$conn) {
+    die("Connection failed: " . mysqli_error());
+  } 
 
-    // Create connection
-    $conn = mysqli_connect($servername, $username, $password, $db);
+  $name = false;
+  $nation = false;
+  $all = false;
+  if(isset($_POST['name'])){
+    $name = $_POST['name'];
+    $sql= "SELECT * FROM `HALAL_RESTAURANTS` WHERE `Nationality` LIKE ('%$name%')";
+  }
+  if(isset($_POST['all_value'])){
+    $sql= "SELECT * FROM `HALAL_RESTAURANTS`";
+  }
 
-    // Check connection
-    if (!$conn) {
-        die("Connection failed: " . mysqli_error());
-    } 
+  // update location with location_status if admin location_status.
+    // $sql= "SELECT * FROM `halal` WHERE `Nationality` LIKE ('$name')";
 
-    $name = false;
-    $nation = false;
-    $all = false;
-    if(isset($_POST['name'])){
-        $name = $_POST['name'];
-        $sql= "SELECT * FROM `halal` WHERE `Nationality` LIKE ('%$name%')";
-    }
-    if(isset($_POST['all_value'])){
-      $sql= "SELECT * FROM `halal`";
-    }
+    $restaurant = mysqli_query($conn, $sql);
 
-    // update location with location_status if admin location_status.
-      // $sql= "SELECT * FROM `halal` WHERE `Nationality` LIKE ('$name')";
-
-      $restaurant = mysqli_query($conn, $sql);
-
-      $geojson = array( 'type' => 'FeatureCollection', 'features' => array());
-      while($row = mysqli_fetch_assoc($restaurant)){
-        if($row['Description']==null){
-          $row['Description']='';
-        }
-        $marker = array(
-                    'type' => 'Feature',
-                    'features' => array(
-                        'type' => 'Feature',
-                        'properties' => array(
-                            'title' => $row['Name'],
-                            'description' => $row['Description'],
-                            'marker-color' => '#f00',
-                            'marker-size' => 'small'
-                            ),
-                        "geometry" => array(
-                            'type' => 'Point',
-                            'coordinates' => array( 
-                                $row['lng'],
-                                $row['lat']
-                            )
-                        )
-                    )
-        );
-      array_push($geojson['features'], $marker['features']);
+    $geojson = array( 'type' => 'FeatureCollection', 'features' => array());
+    while($row = mysqli_fetch_assoc($restaurant)){
+      if($row['Description']==null){
+        $row['Description']='';
       }
-      // echo json_encode($geojson, JSON_NUMERIC_CHECK);
-    $conn->close();
+      $marker = array(
+        'type' => 'Feature',
+        'features' => array(
+            'type' => 'Feature',
+            'properties' => array(
+              'title' => $row['Name'],
+              'description' => $row['Description'],
+              'marker-color' => '#f00',
+              'marker-size' => 'small'
+              ),
+            "geometry" => array(
+              'type' => 'Point',
+              'coordinates' => array( 
+                $row['Longitude'],
+                $row['Latitude']
+              )
+            )
+        )
+      );
+    array_push($geojson['features'], $marker['features']);
+  }
+  // echo json_encode($geojson, JSON_NUMERIC_CHECK);
+  $conn->close();
 
 ?>
